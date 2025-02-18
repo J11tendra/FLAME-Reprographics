844b4945ba1136f0731dddd4e041749163d8e1fd
J11tendra <info.jiitendra@gmail.com>
2025-02-18 21:44:27 +0530

    feat: update UI

--- app.py
+++ app.py
@@ -21,5 +21,8 @@ def home():
     return render_template("login.html")
 
 
+# if __name__ == "__main__":
+#     app.run(host="0.0.0.0", debug=True)
+
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(host='0.0.0.0', port=5000, debug=True)
